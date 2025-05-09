991b37a28bf890e47d24463b1985176b0a067b24
edaros <github@kendar.org>
2025-05-09 15:31:13 +0200

    ### FIX 022: Refactoring

---
+++ my-ai-cms/src/storage.php
@@ -0,0 +1,29 @@
+
+<link rel="stylesheet" href="lib/css/storage.css">
+
+<div class="header">
+    <ul class="breadcrumb" id="breadcrumb">
+        <li><a href="#" data-id="00000000-0000-0000-0000-000000000000">Root</a></li>
+    </ul>
+</div>
+
+<div class="main-container">
+    <div class="tree-panel" id="tree-panel">
+        <!-- Tree nodes will be added here -->
+    </div>
+
+    <div class="content-panel">
+        <div class="content-title" id="content-title">No item selected</div>
+        <textarea class="content-editor" id="content-editor"></textarea>
+        <button class="save-button" id="save-button">Save Content</button>
+    </div>
+</div>
+
+<div class="context-menu" id="context-menu" style="display: none;">
+    <ul>
+        <li id="menu-rename">Rename</li>
+        <li id="menu-delete">Delete</li>
+        <li id="menu-create">Create Child Item</li>
+    </ul>
+</div>
+<script src="lib/scripts/storage.js"></script>
