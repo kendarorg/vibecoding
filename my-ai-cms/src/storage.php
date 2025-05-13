f210addb995ea9df915ac7151452678421c3184c
edaros <github@kendar.org>
2025-05-13 20:29:46 +0200

    ### FIX 044 Let the path works correctly

--- my-ai-cms/src/storage.php
+++ my-ai-cms/src/storage.php
@@ -5,65 +5,8 @@ $dataDir = Settings::$root.'/content/data';
 $structureDir = Settings::$root.'/content/structure';
 $storage = new FlatStorage($dataDir, $structureDir);
 
-// Function to fetch items from the API
-function fetchItems($parentId) {
-    global $storage;
-    $items =$storage->listChildren($parentId);
-    // Add additional information about each item
-    foreach ($items as &$item) {
-        // Check if this item has children
-        $hasChildren = $storage->hasChildren($item['id']);
-        $item['hasChildren'] = $hasChildren;
-
-        // Add parent information explicitly
-        $item['parent'] = $parentId;
-    }
-    return $items;
-}
-
-
-// Function to render the tree recursively
-function renderTree($parentId = '00000000-0000-0000-0000-000000000000', $level = 0) {
-
-    global $session;
-    $openNodes = $session->get('openNodes', []);
-    $items = fetchItems($parentId);
-    $output = '';
-
-    if (empty($items)) {
-        return '<div class="empty-node">No items</div>';
-    }
-
-    foreach ($items as $item) {
-        $id = $item['id'];
-        Utils::errorLog("RENDERING ITEM: $id");
-        $title = htmlspecialchars($item['title']);
-        $hasChildren = $item['hasChildren'] ?? false;
-
-        $isOpen = in_array($id, $openNodes);
-        $toggleChar = $hasChildren ? ($isOpen ? '-' : '+') : ' ';
-        $openClass = $isOpen ? 'open' : '';
-
-        $indent = str_repeat('    ', $level);
-        $output .= "{$indent}<div class=\"tree-node {$openClass}\" data-id=\"{$id}\" data-title=\"{$title}\" data-parent=\"{$parentId}\">\n";
-        $output .= "{$indent}    <span class=\"tree-toggle\">{$toggleChar}</span>\n";
-        $output .= "{$indent}    <span class=\"tree-title\">{$title}</span>\n";
-        $output .= "{$indent}    <div class=\"tree-children\">\n";
-
-        // Recursively render children if the node is open
-        if ($isOpen && $hasChildren) {
-            $output .= renderTree($id, $level + 1);
-        }
-
-        $output .= "{$indent}    </div>\n";
-        $output .= "{$indent}</div>\n";
-    }
-
-    return $output;
-}
-
 // Generate the initial tree structure
-$treeHtml = renderTree();
+$treeHtml = [];
 ?>
 <link rel="stylesheet" href="lib/css/storage.css">
 
